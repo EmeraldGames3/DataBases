b5e9240cfed7bd6eac266d8f54dd202e22411c9f
Bogdan Oprisiu <bobo.oprisiu@gmail.com>
2023-10-31 13:30:19 +0200

    Refactoring

--- Lab2/Querries.sql
+++ Lab2/Querries.sql
@@ -1,7 +1,6 @@
-USE COMPUTER_STORE
+USE COMPUTER_STORE;
 
 /*
-	What do I have:
 		- 5 WHERE
 		- 7 JOIN more than two tables
 		- 1 Outer Joing
@@ -40,13 +39,13 @@ TotalTaxes AS (
     LEFT JOIN Tax t ON ct.tax_ID = t.tax_ID
     GROUP BY c.computer_ID
 )
-SELECT TOP 1
+SELECT TOP 1 WITH TIES
     m.manufacturer_name,
     AVG((1 + ISNULL(tt.total_tax / 100, 0)) * (1 - ISNULL(td.total_discount / 100, 0)) * c.price) AS avg_price
 FROM Manufacturer m
 JOIN Computer c ON m.manufacturer_ID = c.manufacturer_ID
-LEFT JOIN TotalTaxes tt ON c.computer_ID = tt.computer_ID
-LEFT JOIN TotalDiscounts td ON c.computer_ID = td.computer_ID
+JOIN TotalTaxes tt ON c.computer_ID = tt.computer_ID
+JOIN TotalDiscounts td ON c.computer_ID = td.computer_ID
 GROUP BY m.manufacturer_name
 ORDER BY avg_price DESC;
 
@@ -64,10 +63,12 @@ WITH PaymentMethodCounts AS (
 SELECT
     payment_method,
     order_count,
-    (order_count * 100.0) / SUM(order_count) OVER () AS percentage
+    (order_count * 100.0) / (
+        SELECT SUM(order_count)
+        FROM PaymentMethodCounts
+    ) AS percentage
 FROM PaymentMethodCounts
 ORDER BY order_count DESC;
-
 
 
 
@@ -95,8 +96,8 @@ SELECT
     AVG((1 + ISNULL(tt.total_tax / 100, 0)) * (1 - ISNULL(td.total_discount / 100, 0)) * c.price) AS avg_price,
 	MAX((1 + ISNULL(tt.total_tax / 100, 0)) * (1 - ISNULL(td.total_discount / 100, 0)) * c.price) AS max_price
 FROM Computer c
-LEFT JOIN TotalTaxes tt ON c.computer_ID = tt.computer_ID
-LEFT JOIN TotalDiscounts td ON c.computer_ID = td.computer_ID;
+JOIN TotalTaxes tt ON c.computer_ID = tt.computer_ID
+JOIN TotalDiscounts td ON c.computer_ID = td.computer_ID;
 
 
 
@@ -106,35 +107,25 @@ DECLARE @StartDate1 DATE = '2022-01-01';
 DECLARE @EndDate1 DATE = '2022-12-31';
 DECLARE @StartDate2 DATE = '2023-01-01';
 DECLARE @EndDate2 DATE = '2023-12-31';
-WITH TotalTaxes AS (
-    SELECT
-        c.computer_ID,
-        SUM(t.tax_percentage) AS total_tax
-    FROM Computer c
-    LEFT JOIN ComputerTax ct ON c.computer_ID = ct.computer_ID
-    LEFT JOIN Tax t ON ct.tax_ID = t.tax_ID
-    GROUP BY c.computer_ID
-),
-TotalDiscounts AS (
-    SELECT
-        c.computer_ID,
-        SUM(d.discount_percentage) AS total_discount
-    FROM Computer c
-    LEFT JOIN ComputerDiscount cd ON c.computer_ID = cd.computer_ID
-    LEFT JOIN Discount d ON cd.discount_ID = d.discount_ID
-    GROUP BY c.computer_ID
-),
-TotalSales AS (
+WITH TotalSales AS (
     SELECT
         co.order_ID,
-        SUM((1 + ISNULL(t.total_tax / 100, 0)) * (1 - ISNULL(d.total_discount / 100, 0)) * c.price) AS total_price
+        SUM(
+            (1 + ISNULL(t.tax_percentage / 100, 0)) *
+            (1 - ISNULL(d.discount_percentage / 100, 0)) *
+            c.price
+        ) AS total_price
     FROM ComputerOrder co
     JOIN [Order] o ON co.order_ID = o.order_ID
     JOIN Computer c ON co.computer_ID = c.computer_ID
-    LEFT JOIN TotalTaxes t ON c.computer_ID = t.computer_ID
-    LEFT JOIN TotalDiscounts d ON c.computer_ID = d.computer_ID
-    WHERE (o.order_date >= @StartDate1 AND o.order_date <= @EndDate1)
-       OR (o.order_date >= @StartDate2 AND o.order_date <= @EndDate2)
+    LEFT JOIN ComputerTax ct ON c.computer_ID = ct.computer_ID
+    LEFT JOIN Tax t ON ct.tax_ID = t.tax_ID
+    LEFT JOIN ComputerDiscount cd ON c.computer_ID = cd.computer_ID
+    LEFT JOIN Discount d ON cd.discount_ID = d.discount_ID
+    WHERE 
+        (o.order_date >= @StartDate1 AND o.order_date <= @EndDate1)
+        OR 
+        (o.order_date >= @StartDate2 AND o.order_date <= @EndDate2)
     GROUP BY co.order_ID, o.order_date
 )
 SELECT '2022' AS year, SUM(total_price) AS total_sales_revenue
@@ -148,75 +139,30 @@ WHERE o.order_date >= @StartDate2 AND o.order_date <= @EndDate2;
 
 
 
--- Query 5: Find repeat customers that made big purchases
-WITH TotalPricePerComputer AS (
-    SELECT
-        co.order_ID,
-        c.computer_ID,
-        c.price,
-        co.computer_amount,
-        SUM((1 + t.tax_percentage / 100) * ISNULL(1 - d.discount_percentage / 100, 1) * c.price) AS total_price_per_unit
-    FROM ComputerOrder co
-    JOIN Computer c ON co.computer_ID = c.computer_ID
-    LEFT JOIN ComputerTax ct ON c.computer_ID = ct.computer_ID
-    LEFT JOIN Tax t ON ct.tax_ID = t.tax_ID
-    LEFT JOIN ComputerDiscount cd ON c.computer_ID = cd.computer_ID
-    LEFT JOIN Discount d ON cd.discount_ID = d.discount_ID
-    GROUP BY co.order_ID, c.computer_ID, c.price, co.computer_amount
-),
-TotalPricePerOrder AS (
-    SELECT
-        order_ID,
-        SUM(total_price_per_unit * computer_amount) AS total_order_price
-    FROM TotalPricePerComputer
-    GROUP BY order_ID
-),
-RepeatCustomers AS (
-    SELECT c.customer_id
-    FROM Customer c
-    JOIN [Order] o ON c.customer_id = o.customer_id
-    GROUP BY c.customer_id
-    HAVING COUNT(o.order_id) >= 2
-),
-AverageOrderPrice AS (
-    SELECT
-        AVG(tpo.total_order_price) AS average_order_price
-    FROM TotalPricePerOrder tpo
-),
-BigPurchases AS (
-    SELECT c.customer_id
-    FROM Customer c
-    JOIN [Order] o ON c.customer_id = o.customer_id
-    JOIN TotalPricePerOrder tpo ON o.order_ID = tpo.order_ID
-    WHERE tpo.total_order_price >= (SELECT average_order_price FROM AverageOrderPrice)
-    GROUP BY c.customer_id
-),
-TotalMoneySpent AS (
+-- Querry 5: Find repeat customers and how much they spent
+WITH CustomerTotalSpending AS (
     SELECT
         c.customer_id,
-        SUM(tpo.total_order_price) AS total_money_spent
+        c.first_name,
+        c.family_name,
+        SUM((1 + ISNULL(t.tax_percentage / 100, 0)) * (1 - ISNULL(d.discount_percentage / 100, 0)) * com.price) AS total_spending
     FROM Customer c
     JOIN [Order] o ON c.customer_id = o.customer_id
-    JOIN TotalPricePerOrder tpo ON o.order_ID = tpo.order_ID
-    GROUP BY c.customer_id
+    JOIN ComputerOrder co ON o.order_ID = co.order_ID
+    JOIN Computer com ON co.computer_ID = com.computer_ID
+    LEFT JOIN ComputerTax ct ON com.computer_ID = ct.computer_ID
+    LEFT JOIN Tax t ON ct.tax_ID = t.tax_ID
+    LEFT JOIN ComputerDiscount cd ON com.computer_ID = cd.computer_ID
+    LEFT JOIN Discount d ON cd.discount_ID = d.discount_ID
+    GROUP BY c.customer_id, c.first_name, c.family_name
+    HAVING COUNT(o.order_ID) > 1
 )
 SELECT
-    c.customer_id,
-    c.first_name,
-    c.family_name,
-    tms.total_money_spent
-FROM Customer c
-JOIN TotalMoneySpent tms ON c.customer_id = tms.customer_id
-WHERE c.customer_id IN (SELECT customer_id FROM RepeatCustomers)
-INTERSECT
-SELECT
-    c.customer_id,
-    c.first_name,
-    c.family_name,
-    tms.total_money_spent
-FROM Customer c
-JOIN TotalMoneySpent tms ON c.customer_id = tms.customer_id
-WHERE c.customer_id IN (SELECT customer_id FROM BigPurchases);
+    customer_id,
+    first_name,
+    family_name,
+    total_spending
+FROM CustomerTotalSpending;
 
 
 
@@ -279,7 +225,7 @@ WITH MostPopularOS AS (
 SELECT cu.customer_id, cu.first_name, cu.family_name
 FROM Customer cu
 WHERE cu.customer_id = ANY (
-    SELECT DISTINCT o.customer_id
+    SELECT o.customer_id
     FROM [Order] o
     WHERE o.customer_id = cu.customer_id
     AND o.order_ID IN (
