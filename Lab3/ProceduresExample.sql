a82b5813f97b0e720d30c3c7411fc86e212d9f5a
Bogdan Oprisiu <bobo.oprisiu@gmail.com>
2023-11-16 22:51:24 +0200

    .

--- Lab3/ProceduresExample.sql
+++ Lab3/ProceduresExample.sql
@@ -6,7 +6,8 @@ EXEC AddColumnToTable 'Customers', 'Age', 'INT';
 EXEC ChangeColumnType 'Customers', 'CustomerName', 'CHAR(20)';
 EXEC AddDefaultConstraint 'Customers', 'CustomerName', '''Nume''';
 EXEC AddDefaultConstraint 'Customers', 'Age', '18';
-EXEC CreateTable 'Orders', 'OrderID INT PRIMARY KEY, OrderDate DATETIME, CustomerID INT';
+EXEC CreateTable 'Orders', 'OrderID INT PRIMARY KEY, OrderDate DATETIME';
+EXEC AddColumnToTable 'Orders', 'CustomerID', 'INT';
 EXEC AddForeignKeyConstraint 'Orders', 'CustomerID', 'Customers', 'CustomerID';
 
 EXEC ShowVersionHistory;
@@ -15,7 +16,7 @@ EXEC GoToVersion 2;
 EXEC GoToVersion 3;
 EXEC GoToVersion 0;
 EXEC GoToVersion 5;
-EXEC GoToVersion 7;
+EXEC GoToVersion 8;
 
 EXEC GoToVersion 0;
 EXEC RollbackSetUp;
